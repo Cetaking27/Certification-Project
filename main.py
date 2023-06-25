ed39b1c61156d2150242f6ed7d246df4f1fbfec4
Haba Alphonse <alphonsehaba27@gmail.com>
2023-06-26 01:35:26 +0300

    my changes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import telebot
-from telegram.ext import Updater, MessageHandler, filters
 
 
 bot = telebot.TeleBot("6053282566:AAHVdAgIO5KfVyRcHIDbv98ZRsjwL01qp0Y",parse_mode=None)
@@ -11,12 +10,6 @@ def send_welcome(message):
 @bot.message_handler(func=lambda m:True)
 def echo_all(message):
     bot.reply_to(message, message.text)
-    
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-    echo_handler = MessageHandler(filters.text &amp; (~filters.command), echo)
-    bot.dispatcher.add_handler(echo_handler)
 
-    
 
 bot.infinity_polling(none_stop = True)
